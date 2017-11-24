0dd296024ec906d2df2b876248860c3f788a2fb8
komcal <kom.sujautra@gmail.com>
2017-11-25 00:05:46 +0700

    rest server

--- rest/server.js
+++ rest/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { idols, groups } from '../database';
 
 const app = express();
 
@@ -7,6 +8,26 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello, World.');
+});
+
+app.get('/groups', (req, res) => {
+  res.json(groups);
+});
+
+app.get('/groups/:id', (req, res) => {
+  const { id } = req.params;
+  const group = groups.find(group => group.id === id);
+  res.json(group);
+});
+
+app.get('/idols', (req, res) => {
+  res.json(idols);
+});
+
+app.get('/idols/:id', (req, res) => {
+  const { id } = req.params;
+  const idol = idols.find(idol => idol.id === id);
+  res.json(idol);
 });
 
 const PORT = process.env.PORT || 8000;
